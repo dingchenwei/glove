6d56f5e09c2332d064fcbab8f9ba2c4de40143c4
dingchenwei <dingchenwei95@gmail.com>
2017-01-24 23:58:30 +0800

    Initial Commit

---
+++ gloveTests/gloveTests.swift
@@ -0,0 +1,36 @@
+//
+//  gloveTests.swift
+//  gloveTests
+//
+//  Created by 丁晨炜 on 17/1/24.
+//  Copyright © 2017年 zju. All rights reserved.
+//
+
+import XCTest
+@testable import glove
+
+class gloveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
